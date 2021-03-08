b8a994849d92691fa2649f0258fdbbe7c24d1406
alwenarin <arina.mancinimarlot@outlook.fr>
2021-03-08 17:07:39 +0100

    aspectvisuel

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "materialize-css";
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
+
